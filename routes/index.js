69fb3879728484784ad981d5ad85a7290becd3e3
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2014-01-08 00:47:06 +1100

    Add cover photo url

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ exports.createRoutes = function(app_ref){
   app.get('/', musicRoute);
   app.get('/scan', scanRoute);
   app.get('/songs/:id', sendSong);
+  app.get('/cover/:id', sendCover);
   app.io.route('scan_page_connected', function(req){ req.io.join('scanners'); });
   app.io.route('start_scan', function(req){ lib_func.scanLibrary(app); });
   app.io.route('stop_scan', function(req){ lib_func.stopScan(app); });
@@ -25,7 +26,21 @@ function musicRoute(req, res){
 
 function sendSong(req, res){
   app.db.songs.findOne({_id: req.params.id}, function(err, song){
-    res.sendfile(song.location, {'root': '/'});
+    if(err || !song){
+      res.status(404).send();
+    } else {
+      res.sendfile(song.location, {'root': '/'});
+    }
+  });
+}
+
+function sendCover(req, res){
+  app.db.songs.findOne({_id: req.params.id}, function(err, song){
+    if(!song.hasOwnProperty("cover_location")){
+      res.sendfile("/static/images/unknown.png", {root: __dirname + "/../"});
+    } else {
+      res.sendfile(song.cover_location, {'root': '/'});
+    }
   });
 }
 
