f5d3f415d770983faba605e5e33846ca4e0c6c72
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2014-02-23 22:17:40 +1100

    Fix a max width on a coverbox based on the screen size

--- static/js/libs/coverbox.js
+++ static/js/libs/coverbox.js
@@ -1,4 +1,5 @@
 var element = null;
+var padding_factor = 100;
 function CoverBox(url){
   this.url = url;
   this.activate = function(){
@@ -7,11 +8,21 @@ function CoverBox(url){
     $(".modal-backdrop").click(this.deactivate);
     element = $("<img class='modal-picture' style='" + imgCSS + "' src='" + this.url + "' />");
     element.on('load', function(){
-      docW = $(window).width();
-      docH = $(window).height();
-      imgW = this.width;
-      imgH = this.height;
-      element.css({"left": ((docW/2) - (imgW/2)) + "px","top": ((docH/2) - (imgH/2)) + "px"});
+      var docW = $(window).width();
+      var docH = $(window).height();
+      // work out the maximum height (smaller out of width and height)
+      var maxw = docW - padding_factor;
+      var maxh = docH - padding_factor;
+      var max_size = (maxw > maxh) ? maxh : maxw;
+      // calculate the width and height to use
+      var imgW = (this.width < max_size) ? this.width : max_size;
+      var imgH = (this.height < max_size) ? this.height : max_size;
+      element.css({
+        "left": ((docW/2) - (imgW/2)) + "px",
+        "top": ((docH/2) - (imgH/2)) + "px",
+        "max-width:": imgW + "px",
+        "max-height": imgH + "px"
+      });
       $(document.body).append(element);
     });
   }
