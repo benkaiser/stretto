83aababdc73e8fa493b307e88edfc227e11df7c1
Corné Oppelaar <hello@eaterofco.de>
2014-07-23 15:24:31 +0200

    add ignore parameters

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ exports.config = function(){
     // edit below here
     music_dir: "/home/benkaiser/Music/"
     // and above here
+    ignore: [
+      /(\/|^)\./ // ignore dot files and folders
+    ]
   }
   data.music_dir = path.normalize(data.music_dir);
   if(data.music_dir.lastIndexOf("/") == data.music_dir.length-1){
