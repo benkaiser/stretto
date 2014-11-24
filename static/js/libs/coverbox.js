5889ed0a9e64d9d2ada18a380fe9af73fbdc6c04
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2014-11-24 13:26:08 +1100

    fix: patch coverbox to work when screen can't fit image

--- static/js/libs/coverbox.js
+++ static/js/libs/coverbox.js
@@ -1,10 +1,10 @@
 var element = null;
 var padding_factor = 100;
-function CoverBox(url, deactivatedCB){
+function CoverBox(url){
   this.url = url;
   this.activate = function(){
     var imgCSS = "position: fixed; z-index: 1031; border-radius: 20px; box-shadow: #333 0px 0px 30px 5px;";
-    $(document.body).append("<div class='modal-backdrop fade in'></div>");
+    $(document.body).append("<div class='modal-backdrop fade in' style='z-index: 1030;'></div>");
     $(".modal-backdrop").click(this.deactivate);
     element = $("<img class='modal-picture' style='" + imgCSS + "' src='" + this.url + "' />");
     element.on('load', function(){
@@ -13,10 +13,15 @@ function CoverBox(url, deactivatedCB){
       // work out the maximum height (smaller out of width and height)
       var maxw = docW - padding_factor;
       var maxh = docH - padding_factor;
-      var max_size = (maxw > maxh) ? maxh : maxw;
+      // calculate the maximum ratio of available space to image height / width
+      var ratio_w = maxw / this.width;
+      var ratio_h = maxh / this.height;
+      var max_ratio = (ratio_w > ratio_h) ? ratio_h : ratio_w;
       // calculate the width and height to use
-      var imgW = (this.width < max_size) ? this.width : max_size;
-      var imgH = (this.height < max_size) ? this.height : max_size;
+      // if the max_ratio is greater than 1, it means we can show it at full size,
+      // otherwise we must scale it down by the max_ratio
+      var imgW = (max_ratio > 1) ? this.width : this.width * max_ratio;
+      var imgH = (max_ratio > 1) ? this.height : this.height * max_ratio;
       element.css({
         "left": ((docW/2) - (imgW/2)) + "px",
         "top": ((docH/2) - (imgH/2)) + "px",
@@ -29,6 +34,5 @@ function CoverBox(url, deactivatedCB){
   this.deactivate = function(){
     $(".modal-backdrop").remove();
     $(".modal-picture").remove();
-    deactivatedCB();
   };
 }
