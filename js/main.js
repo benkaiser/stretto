ce21cfaf43a72677676bcc8358ad1e8066173ecc
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-01-16 23:06:43 -0800

    initial pages website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var latestRelease = 'https://github.com/benkaiser/stretto/releases/download/v0.0.2/Stretto-';
+  var os;
+
+  if (platform.os.family == 'Linux') {
+    // if it's linux, follow this format
+    latestRelease += platform.os.family.toLowerCase() + '-' + platform.os.architecture + 'bit';
+    os = 'Linux';
+  } else if (platform.os.family.toLowerCase().indexOf('windows') >= 0) {
+    latestRelease += 'windows-' + platform.os.architecture + 'bit';
+    os = 'Windows';
+  } else if (platform.os.family == 'OS X') {
+    latestRelease += 'mac';
+    os = 'Mac OS X';
+  } else {
+    return;
+  }
+
+  latestRelease += '.zip';
+
+  $('.download-main').html('Download for ' + os);
+  $('.download-main').attr('href', latestRelease);
+});
