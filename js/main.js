b5c8df5496595e52b14e18dd563d7b310f2e17bf
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-12-03 22:55:02 +1000

    automatically detect latest release version

--- js/main.js
+++ js/main.js
@@ -10,25 +10,35 @@ var architectureLookup = {
 };
 
 $(document).ready(function () {
-  var latestRelease = 'https://github.com/benkaiser/stretto/releases/download/v0.0.6/Stretto-';
-  var os;
+  getLatestReleaseName(function (name) {
+    var latestRelease = 'https://github.com/benkaiser/stretto/releases/download/' +
+                        name + '/Stretto-';
+    var os;
 
-  if (platform.os.family == 'Linux') {
-    // if it's linux, follow this format
-    latestRelease += platformLookup[platform.os.family.toLowerCase()];
-    os = 'Linux';
-  } else if (platform.os.family.toLowerCase().indexOf('windows') >= 0) {
-    latestRelease += platformLookup.windows;
-    os = 'Windows';
-  } else if (platform.os.family == 'OS X') {
-    latestRelease += platformLookup.mac;
-    os = 'macOS';
-  } else {
-    return;
-  }
+    if (platform.os.family == 'Linux') {
+      // if it's linux, follow this format
+      latestRelease += platformLookup[platform.os.family.toLowerCase()];
+      os = 'Linux';
+    } else if (platform.os.family.toLowerCase().indexOf('windows') >= 0) {
+      latestRelease += platformLookup.windows;
+      os = 'Windows';
+    } else if (platform.os.family == 'OS X') {
+      latestRelease += platformLookup.mac;
+      os = 'macOS';
+    } else {
+      return;
+    }
 
-  latestRelease += '-' + architectureLookup[platform.os.architecture] + '.zip';
+    latestRelease += '-' + architectureLookup[platform.os.architecture] + '.zip';
 
-  $('.download-main').html('Download for ' + os);
-  $('.download-main').attr('href', latestRelease);
+    $('.download-main').html('Download for ' + os);
+    $('.download-main').attr('href', latestRelease);
+  });
 });
+
+function getLatestReleaseName(callback) {
+  $.getJSON('https://api.github.com/repos/benkaiser/stretto/tags').done(function (json) {
+    var release = json[0];
+    callback(release.name);
+  });
+}
