7212f4fefe4746af9d99098aebefe191ca3d0f98
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-12-03 21:31:41 +1000

    upgrade packaging version

--- electronres/buildpackages.sh
+++ electronres/buildpackages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # first run electron-packager and put the output in tmp
 echo 'Building Stretto for all platforms'
-electron-packager ./ "Stretto" --platform=win32,linux,darwin --arch=all --version=0.37.6 --out=/tmp --overwrite --ignore="dbs|bower_components|electronres" --icon electronres/icon --prune
+electron-packager ./ "Stretto" --platform=win32,linux,darwin --arch=all --version=1.4.10 --out=/tmp --overwrite --ignore="dbs|bower_components|electronres" --icon electronres/icon --prune
 
 # zip the resulting Stretto folders
 echo 'Zipping packages for uploading'
