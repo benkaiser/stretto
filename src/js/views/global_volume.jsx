6df3d3121da09df6abc1be4ee1589d0c15ded5c1
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-02-14 17:21:55 +1000

    left-align slider

--- src/js/views/global_volume.jsx
+++ src/js/views/global_volume.jsx
@@ -6,7 +6,7 @@ export default class CountrySwitcher extends React.Component {
     return (
       <div className='form-group'>
         <label className='col-sm-2 control-label' title='used for showing top charts and search'>App Volume</label>
-        <div className='col-sm-10'>
+        <div className='col-sm-2'>
           <VolumeSlider />
         </div>
       </div>
