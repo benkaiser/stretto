b050410b596ac885dc1c846e96afdb62a03039a0
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-09-08 14:50:27 +1000

    Tailer the instructions by platform

--- src/js/services/first_run_experience.jsx
+++ src/js/services/first_run_experience.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Utilities from '../utilities';
 
 import Bootbox from "./bootbox";
 
@@ -15,12 +16,12 @@ export default class FirstRunExperience {
       <div>
         For Stretto to run correctly, we need a chrome extension installed.
         <ol>
-          <li>Download this <a href="https://github.com/benkaiser/Stretto-Helper-Extension/raw/master/extension.zip">chrome extension zip file</a></li>
-          <li>Unzip the file</li>
-          <li>Navigate to `chrome://extensions` in your browser</li>
+          <li>Download the <a href="https://github.com/benkaiser/Stretto-Helper-Extension/raw/master/extension.zip">extension zip file</a></li>
+          { !Utilities.isMobile() && <li>Unzip the file</li> }
+          { Utilities.isMobile() ? <li>Navigate to `kiwi://extensions` in your browser</li> : <li>Navigate to `chrome://extensions` in your browser</li> }
           <li>Enable "Developer mode" in the top right corner</li>
-          <li>Click "Load unpacked" and select the unzipped extension folder</li>
-          <li>Reload this page</li>
+          { Utilities.isMobile() ? <li>Click "+ (from zip/...)" and select the extension zip file</li> : <li>Click "Load unpacked" and select the unzipped extension folder</li> }
+          <li>Reload Stretto</li>
         </ol>
       </div>
     );
